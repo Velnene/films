696ee1ebc80ee64490af5c9e9d6cef4034069ace
Viktor <viktor.hidirov@yandex.ru>
2023-04-05 22:53:57 +0700

    feat: поиск без нажатия на интер

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,17 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import api from '../utils/api';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 function Header(props) {
-const [value, getValue] = useState('')
-  
+  const [value, getValue] = useState('')
+
   function handleSubmit(e) {
     e.preventDefault();
     props.getSerchFilm(value)
   }
+
+  useEffect(() => {
+    props.getSerchFilm(value)
+  }, [value])
 
   function handleChange(e) {
     getValue(e.target.value)
@@ -15,7 +19,6 @@ const [value, getValue] = useState('')
 
   return (
     <header className="header">
-
       <div className='header__logo'>Header</div>
       <form onSubmit={handleSubmit}>
         <input value={value} onChange={handleChange} type='text' placeholder='Поиск' className='header__search' />
